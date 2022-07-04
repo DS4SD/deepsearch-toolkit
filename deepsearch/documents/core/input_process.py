a63dcfe62a6e789be924b46252a83928ef750f2c
Lokesh Mishra <exomishra@gmail.com>
2022-07-04 21:38:53 +0200

    Modified default behaviour for cli vs non cli use
    Signed-off-by: Lokesh Mishra <exomishra@gmail.com>

--- deepsearch/documents/core/input_process.py
+++ deepsearch/documents/core/input_process.py
@@ -14,13 +14,15 @@ from tqdm import tqdm
 
 from deepsearch.cps.client.api import CpsApi
 from deepsearch.cps.client.components.documents import DocumentConversionResult
-from .utils import batch_single_files
+from .utils import batch_single_files, create_root_dir
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from .common_routines import ERROR_MSG, progressbar_padding, success_message
 from .convert import (
     check_status_running_tasks,
+    download_converted_documents,
+    get_download_url,
     send_files_for_conversion,
     send_urls_for_conversion,
 )
@@ -34,7 +36,7 @@ url_public_apis = "/public/v4"
 
 
 def process_local_input(
-    api: CpsApi, cps_proj_key: str, local_file: Path
+    api: CpsApi, cps_proj_key: str, local_file: Path, progress_bar=False, cli_use=False
 ) -> DocumentConversionResult:
     """
     Classify the user provided local input and take appropriate action.
@@ -43,22 +45,46 @@ def process_local_input(
         logger.error("Error: File not found. Check input.")
     else:
         with tempfile.TemporaryDirectory() as tmpdir:
-            batched_files = batch_single_files(local_file=local_file, root_dir=tmpdir)
+            batched_files = batch_single_files(
+                local_file=local_file, root_dir=tmpdir, progress_bar=progress_bar
+            )
             task_ids = send_files_for_conversion(
                 api=api,
                 cps_proj_key=cps_proj_key,
                 local_file=local_file,
                 root_dir=tmpdir,
+                progress_bar=progress_bar,
             )
             statuses = check_status_running_tasks(
-                api=api, cps_proj_key=cps_proj_key, task_ids=task_ids
+                api=api,
+                cps_proj_key=cps_proj_key,
+                task_ids=task_ids,
+                progress_bar=progress_bar,
             )
-        return DocumentConversionResult(
-            proj_key=cps_proj_key,
-            task_ids=task_ids,
-            statuses=statuses,
-            source_file=local_file,
-        )
+        if cli_use == False:
+            return DocumentConversionResult(
+                proj_key=cps_proj_key,
+                task_ids=task_ids,
+                statuses=statuses,
+                source_file=local_file,
+            )
+        else:
+            result_dir = create_root_dir()
+            urls = get_download_url(
+                cps_proj_key=cps_proj_key,
+                task_ids=task_ids,
+                api=api,
+            )
+            download_converted_documents(
+                download_urls=urls, result_dir=result_dir, progress_bar=True
+            )
+            logger.info("%s\nResults: %s", success_message, os.path.abspath(result_dir))
+            report_docs(
+                result_dir=result_dir,
+                task_ids=task_ids,
+                statuses=statuses,
+                local_file=local_file,
+            )
 
 
 def process_urls_input(api: CpsApi, cps_proj_key: str, urls: List[str]):
