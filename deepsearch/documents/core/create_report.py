f04e1be9ed2398f289d9de5e5ad66c1944f4dbd9
Lokesh Mishra <exomishra@gmail.com>
2022-07-04 17:14:37 +0200

    Updated + Removed time functionality from report
    - current implementation of time functionality not compatible with doc conversion updates
    
    Signed-off-by: Lokesh Mishra <exomishra@gmail.com>

--- deepsearch/documents/core/create_report.py
+++ deepsearch/documents/core/create_report.py
@@ -1,40 +1,27 @@
 import csv
-import datetime
 import glob
 import os
 import pathlib
-import time
+import tempfile
 from pathlib import Path
 from typing import Any, List
 
-import typer
+from .utils import batch_single_files
 
 
 def report_urls(
-    root_dir: Path, urls: List[str], statuses: List[str], task_ids: List[str]
+    result_dir: Path, urls: List[str], statuses: List[str], task_ids: List[str]
 ):
     """
     Function to create report when DeepSearch is converting urls.
     """
-    report_name = os.path.join(root_dir, "report.csv")
-    time_s = os.path.basename(os.path.abspath(root_dir)).lstrip("results_")
-    time_start = time.mktime(
-        datetime.datetime.strptime(time_s, "%Y-%m-%d_%Hh%Mm%Ss").timetuple()
-    )
-    total_time = time.time() - time_start
+    report_name = os.path.join(result_dir, "report.csv")
 
     pad = 35
-    typer.echo(f"\n{'Total online documents:':<{pad}}{len(urls):04}")
-    typer.echo(
+    print(f"\n{'Total online documents:':<{pad}}{len(urls):04}")
+    print(
         f"""{'Successfully converted documents:':<{pad}}{statuses.count('SUCCESS'):04}({int(100*statuses.count('SUCCESS')/len(urls))}%)"""
     )
-    if total_time < 60:
-        typer.echo(f"{'Run time:':<{pad}}{total_time:.2f} seconds")
-    elif total_time > 60 and total_time < 3600:
-        typer.echo(f"{'Run time:':<{pad}}{total_time/60:.2f} minutes")
-    else:
-        typer.echo(f"{'Run time:':<{pad}}{total_time/3600:.2f} hours")
-
     with open(report_name, mode="a", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["s. no.", "task_id", "status", "url"])
@@ -45,8 +32,7 @@ def report_urls(
 
 
 def report_docs(
-    root_dir: Path,
-    batched_files: List[List[str]],
+    result_dir: Path,
     statuses: List[str],
     task_ids: List[str],
     local_file: Path,
@@ -54,51 +40,36 @@ def report_docs(
     """
     Function to create report when DeepSearch is converting local documents.
     """
-    report_name = os.path.join(root_dir, "report.csv")
-    time_s = os.path.basename(os.path.abspath(root_dir)).lstrip("results_")
-    time_start = time.mktime(
-        datetime.datetime.strptime(time_s, "%Y-%m-%d_%Hh%Mm%Ss").timetuple()
-    )
-    total_time = time.time() - time_start
+    report_name = os.path.join(result_dir, "report.csv")
 
-    # batched_files only contains information about single pdfs
-    # user zips are collected again
-    files_zip: List[Any] = []
-    if os.path.isdir(local_file):
-        files_zip = glob.glob(os.path.join(local_file, "**/*.zip"), recursive=True)
-    elif os.path.isfile(local_file):
-        file_extension = pathlib.Path(local_file).suffix
-        if file_extension == ".zip":
-            files_zip = [local_file]
+    with tempfile.TemporaryDirectory() as tmpdir:
+        batched_files = batch_single_files(local_file=local_file, root_dir=tmpdir)
 
-    count_total_docs = len(batched_files) + len(files_zip)
+        # batched_files only contains information about single pdfs
+        # user zips are collected again
+        files_zip: List[Any] = []
+        if os.path.isdir(local_file):
+            files_zip = glob.glob(os.path.join(local_file, "**/*.zip"), recursive=True)
+        elif os.path.isfile(local_file):
+            file_extension = pathlib.Path(local_file).suffix
+            if file_extension == ".zip":
+                files_zip = [local_file]
 
-    if root_dir is not None:
+        count_total_docs = len(batched_files) + len(files_zip)
+
+        # count batched zips
         files_tmpzip = glob.glob(
-            os.path.join(root_dir, "tmpzip/**/*.zip"), recursive=True
+            os.path.join(tmpdir, "tmpzip/**/*.zip"), recursive=True
         )
         files_zip = files_zip + files_tmpzip
-    # catch document names from batched_files
-    # docs = []
-    # for file, batch in batched_files:
-    #     if batch in files_zip:
-    #         doc.append(file)
-    #     else:
-    #         doc.append()
-    # doc_file, batch_name = zip(*batched_files)
 
     pad = 35
-    typer.echo(f"\n{'Total files (pdf+zip):':<{pad}}{count_total_docs:04}")
-    typer.echo(f"{'Total batches:':<{pad}}{len(files_zip):04}")
-    typer.echo(
+    print(f"\n{'Total files (pdf+zip):':<{pad}}{count_total_docs:04}")
+    print(f"{'Total batches:':<{pad}}{len(files_zip):04}")
+    print(
         f"""{'Successfully converted batches:':<{pad}}{statuses.count('SUCCESS'):04}({int(100*statuses.count('SUCCESS')/len(files_zip))}%)"""
     )
-    if total_time < 60:
-        typer.echo(f"{'Run time:':<{pad}}{total_time:.2f} seconds")
-    elif total_time > 60 and total_time < 3600:
-        typer.echo(f"{'Run time:':<{pad}}{total_time/60:.2f} minutes")
-    else:
-        typer.echo(f"{'Run time:':<{pad}}{total_time/3600:.2f} hours")
+
     batch_done = []
     with open(report_name, mode="a", newline="") as csvfile:
         writer = csv.writer(csvfile)
