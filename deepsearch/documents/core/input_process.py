67af4c822ac30a882c7840ce57ad0022e90ff326
Lokesh Mishra <exomishra@gmail.com>
2022-07-04 14:35:48 +0200

    modified code for processing local input
    - use tempdir
    - function returns object of DocumentConversionResult
    
    Signed-off-by: Lokesh Mishra <exomishra@gmail.com>

--- deepsearch/documents/core/input_process.py
+++ deepsearch/documents/core/input_process.py
@@ -3,6 +3,7 @@ import glob
 import logging
 import os
 import pathlib
+import tempfile
 import zipfile as z
 from pathlib import Path
 from typing import Any, List
@@ -11,6 +12,7 @@ import urllib3
 from tqdm import tqdm
 
 from deepsearch.cps.client.api import CpsApi
+from deepsearch.cps.client.components.documents import DocumentConversionResult
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -30,34 +32,40 @@ url_user_management = "/user/v1"
 url_public_apis = "/public/v4"
 
 
-def process_local_input(api: CpsApi, cps_proj_key: str, local_file: Path):
+def process_local_input(
+    api: CpsApi, cps_proj_key: str, local_file: Path
+) -> DocumentConversionResult:
     """
     Classify the user provided local input and take appropriate action.
     """
     if not os.path.exists(local_file):
         logger.error("Error: File not found. Check input.")
     else:
-        root_dir = create_root_dir()
-        batched_files = batch_single_files(local_file=local_file, root_dir=root_dir)
-        task_ids = send_files_for_conversion(
-            api=api, cps_proj_key=cps_proj_key, local_file=local_file, root_dir=root_dir
+        # root_dir = create_root_dir()
+        with tempfile.TemporaryDirectory() as tmpdir:
+            batched_files = batch_single_files(local_file=local_file, root_dir=tmpdir)
+            task_ids = send_files_for_conversion(
+                api=api,
+                cps_proj_key=cps_proj_key,
+                local_file=local_file,
+                root_dir=tmpdir,
+            )
+            statuses = check_status_running_tasks(
+                api=api, cps_proj_key=cps_proj_key, task_ids=task_ids
+            )
+
+            # logger.info("%s\nResults: %s", success_message, os.path.abspath(root_dir))
+            # report_docs(
+            #     root_dir=root_dir,
+            #     batched_files=batched_files,
+            #     task_ids=task_ids,
+            #     statuses=statuses,
+            #     local_file=local_file,
+            # )
+            # cleanup(root_dir=root_dir)
+        return DocumentConversionResult(
+            proj_key=cps_proj_key, task_ids=task_ids, statuses=statuses
         )
-        statuses = check_status_running_tasks(
-            api=api, cps_proj_key=cps_proj_key, task_ids=task_ids
-        )
-        download_converted_docs(
-            api=api, cps_proj_key=cps_proj_key, task_ids=task_ids, root_dir=root_dir
-        )
-        logger.info("%s\nResults: %s", success_message, os.path.abspath(root_dir))
-        report_docs(
-            root_dir=root_dir,
-            batched_files=batched_files,
-            task_ids=task_ids,
-            statuses=statuses,
-            local_file=local_file,
-        )
-        cleanup(root_dir=root_dir)
-        return
 
 
 def create_root_dir() -> Path:
