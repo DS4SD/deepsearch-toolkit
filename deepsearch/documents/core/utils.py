9ba51e4a1e5750b709d5f1b56cf420a2a3fa04d6
Lokesh Mishra <exomishra@gmail.com>
2022-07-04 17:11:51 +0200

    Restructure: Moved utility code to utils.py
    Signed-off-by: Lokesh Mishra <exomishra@gmail.com>

---
+++ deepsearch/documents/core/utils.py
@@ -0,0 +1,127 @@
+import datetime
+import requests
+from tqdm import tqdm
+import zipfile as z
+import glob
+import os
+from pathlib import Path
+import pathlib
+from typing import Any, List
+from .common_routines import progressbar_padding
+
+
+def batch_single_files(local_file: Path, root_dir: Path) -> List[List[str]]:
+    """
+    Batch individual pdfs into zip files.
+    """
+    MAX_BATCH_SIZE = 20 * 1e6  # 20 Mb
+    zipdir = os.path.join(root_dir, "tmpzip/")
+    # create directory for batches
+    if not os.path.isdir(zipdir):
+        os.makedirs(zipdir)
+
+    # clear previous tmpzips
+    previous_tmpzips = glob.glob(os.path.join(zipdir, "**/*.zip"), recursive=True)
+    for f in previous_tmpzips:
+        os.remove(os.path.abspath(f))
+
+    # set up zip archive
+    zipfilenumber = 0
+    zipfilename = f"{0:04}{zipfilenumber}.zip"
+    current_zipbatch = zipdir + zipfilename
+
+    # get input pdf files
+    files_pdf: List[Any] = []
+    if os.path.isdir(local_file):
+        files_pdf = glob.glob(os.path.join(local_file, "**/*.pdf"), recursive=True)
+    elif os.path.isfile(local_file):
+        file_extension = pathlib.Path(local_file).suffix
+        if file_extension == ".pdf":
+            files_pdf = [local_file]
+
+    # catch all filenames and batch names
+    batched_files = []
+    if len(files_pdf) != 0:
+        with tqdm(
+            total=len(files_pdf),
+            desc=f'{"Processing input:":<{progressbar_padding}}',
+        ) as progress:
+            # loop over pdfs
+            for single_doc in files_pdf:
+                # check size of current zip file
+                try:
+                    if os.path.getsize(current_zipbatch) > MAX_BATCH_SIZE:
+                        zipfilenumber += 1
+                        zipfilename = f"{0:04}{zipfilenumber}.zip"
+                        current_zipbatch = zipdir + zipfilename
+                except FileNotFoundError:
+                    pass
+                # build name to avoid duplicate names inside batch
+                if len(files_pdf) > 1:
+                    arcname = str(single_doc)[
+                        len(os.path.commonpath(files_pdf)) + 1 :
+                    ].replace("/", "__")
+                else:
+                    arcname = os.path.basename(single_doc)
+
+                with z.ZipFile(current_zipbatch, mode="a") as zipf:
+                    zipf.write(filename=single_doc, arcname=arcname)
+                # store batch/file name for creating report
+                batched_files.append([arcname, current_zipbatch])
+                progress.update(1)
+
+    return batched_files
+
+
+def create_root_dir() -> Path:
+    """
+    Creates root directory labelled with timestamp
+    """
+    # get timestamp
+    ts = datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")
+    root_dir = Path(f"./results_{ts}/")
+
+    if not os.path.isdir(root_dir):
+        os.makedirs(root_dir)
+
+    return root_dir
+
+
+def cleanup(root_dir: Path):
+    """
+    Clean temporarily created zip batches.
+    """
+    import shutil
+
+    zipdir = os.path.join(root_dir, "tmpzip")
+    try:
+        shutil.rmtree(zipdir)
+    except OSError:
+        pass
+
+    # check if rootdir is empty. if yes, then delete it
+    if len(os.listdir(root_dir)) == 0:
+        shutil.rmtree(root_dir)
+
+    return
+
+
+def download_url(url: str, save_path: Path, chunk_size=128):
+    """
+    Download contents from a url.
+    """
+    r = requests.get(url, stream=True, verify=False)
+    with open(save_path, "wb") as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fd.write(chunk)
+    return
+
+
+def get_urls(path: Path) -> List[str]:
+    """
+    Returns list of url from input file.
+    """
+
+    lines = path.read_text()
+    urls = [line.strip() for line in lines.split("\n") if line.strip() != ""]
+    return urls
