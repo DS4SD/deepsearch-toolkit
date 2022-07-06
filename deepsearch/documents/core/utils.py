4a5d86730b82a938ce0d7a6ab88d9c9e13845052
Lokesh Mishra <exomishra@gmail.com>
2022-07-06 10:43:37 +0200

    Added class for url black magic
    Signed-off-by: Lokesh Mishra <exomishra@gmail.com>

--- deepsearch/documents/core/utils.py
+++ deepsearch/documents/core/utils.py
@@ -8,6 +8,24 @@ from pathlib import Path
 import pathlib
 from typing import Any, List
 from .common_routines import progressbar_padding
+from deepsearch.cps.client.api import CpsApi
+
+
+class URLNavigator:
+    api = CpsApi.default_from_env()
+
+    def __init__(self) -> None:
+        self.url_host = api.client.swagger_client.configuration.host
+        self.url_linked_ccs = url_host.rstrip("/public/v1").rstrip("cps") + "linked-ccs"
+        self.url_user_management = "/user/v1"
+        self.url_public_apis = "/public/v4"
+
+    def url_request_status(self, ccs_proj_key, task_id):
+        wait = 5
+        return f"{self.url_linked_ccs}{self.url_public_apis}/projects/{ccs_proj_key}/tasks/{task_id}/status?wait={wait}"
+
+    def url_convert(self, ccs_proj_key):
+        return f"{self.url_linked_ccs}{self.url_public_apis}/projects/{ccs_proj_key}/pipelines/convert"
 
 
 def batch_single_files(
