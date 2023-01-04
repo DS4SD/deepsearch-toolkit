207d80b59501d8e378cfa9903e396da25845c1d8
Panos Vagenas <35837085+vagenas@users.noreply.github.com>
2023-01-04 16:26:47 +0100

    chore: regenerate swagger client from API v6.14.4
    Signed-off-by: Panos Vagenas <35837085+vagenas@users.noreply.github.com>

--- deepsearch/cps/apis/user/api/projects_api.py
+++ deepsearch/cps/apis/user/api/projects_api.py
@@ -1365,6 +1365,130 @@ class ProjectsApi(object):
             _request_timeout=local_var_params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def list_users_paginated(self, proj_key, **kwargs):  # noqa: E501
+        """list_users_paginated  # noqa: E501
+
+        List all users belonging to a cps project  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.list_users_paginated(proj_key, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool: execute request asynchronously
+        :param str proj_key: The project key (required)
+        :param int page: page of the result list
+        :param int items_per_page: items on one page of the result list
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is True.
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :return: dict(str, list[UserDetails])
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        return self.list_users_paginated_with_http_info(proj_key, **kwargs)  # noqa: E501
+
+    def list_users_paginated_with_http_info(self, proj_key, **kwargs):  # noqa: E501
+        """list_users_paginated  # noqa: E501
+
+        List all users belonging to a cps project  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.list_users_paginated_with_http_info(proj_key, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool: execute request asynchronously
+        :param str proj_key: The project key (required)
+        :param int page: page of the result list
+        :param int items_per_page: items on one page of the result list
+        :param _return_http_data_only: response data without head status code
+                                       and headers
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is True.
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :return: tuple(dict(str, list[UserDetails]), status_code(int), headers(HTTPHeaderDict))
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        local_var_params = locals()
+
+        all_params = [
+            'proj_key',
+            'page',
+            'items_per_page'
+        ]
+        all_params.extend(
+            [
+                'async_req',
+                '_return_http_data_only',
+                '_preload_content',
+                '_request_timeout'
+            ]
+        )
+
+        for key, val in six.iteritems(local_var_params['kwargs']):
+            if key not in all_params:
+                raise ApiTypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method list_users_paginated" % key
+                )
+            local_var_params[key] = val
+        del local_var_params['kwargs']
+        # verify the required parameter 'proj_key' is set
+        if self.api_client.client_side_validation and ('proj_key' not in local_var_params or  # noqa: E501
+                                                        local_var_params['proj_key'] is None):  # noqa: E501
+            raise ApiValueError("Missing the required parameter `proj_key` when calling `list_users_paginated`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'proj_key' in local_var_params:
+            path_params['proj_key'] = local_var_params['proj_key']  # noqa: E501
+
+        query_params = []
+        if 'page' in local_var_params and local_var_params['page'] is not None:  # noqa: E501
+            query_params.append(('page', local_var_params['page']))  # noqa: E501
+        if 'items_per_page' in local_var_params and local_var_params['items_per_page'] is not None:  # noqa: E501
+            query_params.append(('items_per_page', local_var_params['items_per_page']))  # noqa: E501
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/projects/{proj_key}/users_paginated', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='dict(str, list[UserDetails])',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=local_var_params.get('async_req'),
+            _return_http_data_only=local_var_params.get('_return_http_data_only'),  # noqa: E501
+            _preload_content=local_var_params.get('_preload_content', True),
+            _request_timeout=local_var_params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def remove_user(self, proj_key, username, **kwargs):  # noqa: E501
         """remove_user  # noqa: E501
 
