275ec5d23157fecfab41faca4b4159eb9e13c2f8
miguel <miguel.brandao@ibm.com>
2023-05-23 10:51:16 +0100

    artifact downloading, library management, list downloaded artifacts and delete them
    Signed-off-by: miguel <miguel.brandao@ibm.com>

--- deepsearch/model/model_download/model_download_typer.py
+++ deepsearch/model/model_download/model_download_typer.py
@@ -11,7 +11,9 @@ from .model_download import (
     infer_target_directory,
     process_downloaded_file,
     default_cache_location,
-    default_download_directory
+    default_download_directory,
+    infer_cache_directory,
+    get_artifacts_in_cache,
 )
 
 model_download_app = typer.Typer(no_args_is_help=True, add_completion=False)
@@ -30,7 +32,7 @@ def download(
     if list_models:
         artifacts = get_artifacts_in_store(artifact_store)
         for artifact in artifacts:
-            typer.echo(artifact[1])
+            typer.echo(artifact["folder_name"])
     elif model is not None:
         model_meta = get_model_meta(artifact_store, model)
         typer.echo(json.dumps(model_meta, indent=4, separators=(",", ": ")))
@@ -39,14 +41,34 @@ def download(
             model_meta, default_download_directory
         )
         typer.echo(f"Extracting {model} to {default_cache_location}")
-        process_downloaded_file(downloaded_file_path, default_cache_location, model)
+        process_downloaded_file(downloaded_file_path, default_cache_location, model, artifact_store)
         typer.echo(f"Cleaning up {default_download_directory}")
         shutil.rmtree(default_download_directory)
 
 
 @model_download_app.command()
-def manage():
-    typer.echo("Executing 'manage' command.")
+def manage(
+    list_models: bool = typer.Option(False, "--list", "-l"),
+    del_model: str = typer.Option(None, "--delete", "-d")
+):
+    cache_dir = infer_cache_directory()
+    artifact_list = get_artifacts_in_cache(cache_dir)
+
+    typer.echo(f"Infered artifact directory {cache_dir}")
+
+    if list_models:
+        for artifact in artifact_list:
+            typer.echo(artifact["folder_name"])
+
+    if del_model is not None:
+        target_artifacts = []
+        for artifact in artifact_list:
+            if "folder_name" in artifact and artifact["folder_name"] == del_model:
+                target_artifacts.append(artifact)
+
+        for artifact in target_artifacts:
+            shutil.rmtree(artifact["full_path"])
+            typer.echo(f"Succefully deleted {artifact['folder_name']}")
 
 
 if __name__ == "__main__":
