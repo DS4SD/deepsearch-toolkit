d75818aa3214885bfb1065f6343bd58ba49ce83c
miguel <miguel.brandao@ibm.com>
2023-05-31 11:06:31 +0100

    Harmonized concept naming throughout the code
    Signed-off-by: miguel <miguel.brandao@ibm.com>

--- deepsearch/model/model_download/artifact_management_typer.py
+++ deepsearch/model/model_download/artifact_management_typer.py
@@ -6,37 +6,37 @@ import typer
 
 from deepsearch.model.model_download.artifact_manager import ArtifactManager
 
-model_download_app = typer.Typer(no_args_is_help=True, add_completion=False)
+artifact_download_app = typer.Typer(no_args_is_help=True, add_completion=False)
 artifact_manager = ArtifactManager()
 
 
-@model_download_app.command()
+@artifact_download_app.command()
 def download(
-    model: str = typer.Option(None, "--model", "-m"),
-    list_models: bool = typer.Option(False, "--list", "-l"),
+    artifact: str = typer.Option(None, "--artifact", "-a"),
+    list_artifacts: bool = typer.Option(False, "--list", "-l"),
 ):
 
-    if list_models:
-        artifacts = artifact_manager.get_artifact_store_list()
+    if list_artifacts:
+        artifacts = artifact_manager.get_index_artifact_list()
         for artifact in artifacts:
             typer.echo(artifact["folder_name"])
-    elif model is not None:
-        artifact_manager.download_artifact_to_cache(model, with_progess_bar=True)
+    elif artifact is not None:
+        artifact_manager.download_artifact_to_cache(artifact, with_progess_bar=True)
 
 
-@model_download_app.command()
+@artifact_download_app.command()
 def manage(
-    list_models: bool = typer.Option(False, "--list", "-l"),
-    del_model: str = typer.Option(None, "--delete", "-d"),
+    list_artifacts: bool = typer.Option(False, "--list", "-l"),
+    del_artifact: str = typer.Option(None, "--delete", "-d"),
 ):
-    if list_models:
+    if list_artifacts:
         for artifact in artifact_manager.get_artifact_cache_list():
             typer.echo(artifact["folder_name"])
 
-    if del_model is not None:
+    if del_artifact is not None:
         target_artifacts = []
         for artifact in artifact_manager.get_artifact_cache_list():
-            if "folder_name" in artifact and artifact["folder_name"] == del_model:
+            if "folder_name" in artifact and artifact["folder_name"] == del_artifact:
                 target_artifacts.append(artifact)
 
         for artifact in target_artifacts:
@@ -45,4 +45,4 @@ def manage(
 
 
 if __name__ == "__main__":
-    model_download_app()
+    artifact_download_app()
