5efa09560e11d0cd67f538b873b29702690e6d11
Miguel Brandão <555migalves555@gmail.com>
2023-08-02 11:16:52 +0100

    pre-commit
    Signed-off-by: Miguel Brandão <555migalves555@gmail.com>

--- deepsearch/cps/cli/data_indices_typer.py
+++ deepsearch/cps/cli/data_indices_typer.py
@@ -165,13 +165,11 @@ def upload_files(
         s3_coordinates=cos_coordinates,
     )
 
-    if all([status=="SUCCESS" for status in statuses]):
+    if all([status == "SUCCESS" for status in statuses]):
         typer.echo("File upload completed successfully")
     else:
         # TODO
         typer.echo("Some files did not upload successfully")
-
-
 
 
 @app.command(
