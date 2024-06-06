c94834e43af3522f51347361464c362e7d15b368
Peter W. J. Staar <91719829+PeterStaar-IBM@users.noreply.github.com>
2024-06-06 13:38:02 +0200

    added the export to HTML (#177)
    The goal is to have an HTML export from the PDF-conversion. Started from
    the original code we used to have in the export.
    
    ---------
    
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- test/test_documents_core_export.py
+++ test/test_documents_core_export.py
@@ -2,15 +2,29 @@ import json
 import os
 from typing import Any
 
-from deepsearch.documents.core.export import export_to_markdown
+from deepsearch.documents.core.export import export_to_markdown, export_to_html
 
 def test_export_to_markdown():
     doc: dict[str, Any] | None = None
     root = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(root, "inputs", "export_to_markdown.json")) as file_doc:
+    with open(os.path.join(root, "inputs", "test_document.json")) as file_doc:
         doc = json.load(file_doc)
     assert doc
     assert isinstance(doc, dict)
 
     md = export_to_markdown(doc)
     assert md
+
+    """
+    with open(os.path.join(root, "inputs", "test_document.md"), "w") as fw:
+        fw.write(md)
+    """
+    
+    html = export_to_html(doc)
+    assert html
+
+    """
+    with open(os.path.join(root, "inputs", "test_document.html"), "w") as fw:
+        fw.write(html)
+    """
+    
