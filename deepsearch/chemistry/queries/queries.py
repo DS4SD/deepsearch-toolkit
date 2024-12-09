596d83db168351361416e4795da2caa69b78cf2f
DKL <dkl@zurich.ibm.com>
2024-12-09 13:09:26 +0100

    Add inchikeys to compound query ids.
    Signed-off-by: DKL <dkl@zurich.ibm.com>

--- deepsearch/chemistry/queries/queries.py
+++ deepsearch/chemistry/queries/queries.py
@@ -24,6 +24,7 @@ class DocumentsQuery(ChemistryQuery):
 class CompoundsByIds(CompoundsQuery):
     """Query compounds that have any of the given identifiers."""
 
+    inchikeys: list[str] = []
     persistent_ids: list[str] = []
 
 
